27ba1078a2934a247038d3d9dc51eeda84acb310
zhangsan <zhangsan@itcast.com>
2019-07-26 19:02:35 +0800

    parent的更新

--- maven-day2-parent/Gittest/src/News.java
+++ maven-day2-parent/Gittest/src/News.java
@@ -3,5 +3,9 @@ public class News {
         System.out.println("new Begin...");
         System.out.println("let me try...");
         System.out.println("check this out..");
+        System.out.println("小贼,此乃来自maven-day2-parent的更新.接招吧.....");
+        for (int i =1;i<5;i++){
+            System.out.println("想你的第"+i+"天");
+        }
     }
 }
