30c57c64e2bbcbf7f6833e4359ab9a9d09c360f6
zhangsan <zhangsan@itcast.com>
2019-07-26 16:54:35 +0800

    敲了点代码..

--- maven-day2-parent/maven-day2-dao/src/main/java/com.itheima.dao/HelloWorld.java
+++ maven-day2-parent/maven-day2-dao/src/main/java/com.itheima.dao/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Let't try it...");
         System.out.println("skywalker...");
+        System.out.println("是谁?");
     }
 }
