8ae28dde8d0db88d74fe543f24bb26feb28ae50f
zhangsan <zhangsan@itcast.com>
2019-07-26 18:58:03 +0800

    试试

--- maven-day2-parent/Gittest/src/News.java
+++ maven-day2-parent/Gittest/src/News.java
@@ -1,5 +1,7 @@
 public class News {
     public static void main(String[] args) {
         System.out.println("new Begin...");
+        System.out.println("let me try...");
+        System.out.println("check this out..");
     }
 }
